8870cf5c3088d3b674705e279de3d45da890992b
nency <nencywadhvana@gmail.com>
2025-01-24 15:50:33 +0530

    Handle case of new delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,6 +2,9 @@ class Calculator
 
   def add(inputs)
     return 0 if inputs.empty?
+    if inputs.start_with?("//")
+      inputs = inputs.gsub(';', ',')
+    end
     inputs.gsub("\n", ",").split(",").map(&:to_i).sum
   end
 
