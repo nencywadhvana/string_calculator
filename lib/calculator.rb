3db33ef59e36e4262a7060df84dc58bfdc3fd17d
nency <nencywadhvana@gmail.com>
2025-01-25 18:53:30 +0530

    Handle case of any length delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,10 +2,16 @@ class Calculator
 
   def add(inputs)
     return 0 if inputs.empty?
+    delimeter = ","
     if inputs.start_with?("//")
-      inputs = inputs.gsub(';', ',')
+      if inputs.start_with?("//[")
+        delimeter = inputs.match(%r{//\[(.*?)\]})[1]
+        inputs = inputs.split("\n")[1]
+      else
+        inputs = inputs.gsub(';', ',')
+      end
     end
-    input_array = inputs.gsub("\n", ",").split(",").map(&:to_i)
+    input_array = inputs.gsub("\n", delimeter).split(delimeter).map(&:to_i)
     find_negatives(input_array)  #find out negative numbers
     input_array.select { |number| number <= 1000}.sum
   end
