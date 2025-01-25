d68d8d14a3d0d54df8b14043298b1a40c3421255
nency <nencywadhvana@gmail.com>
2025-01-25 13:30:46 +0530

    refactor code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,9 +6,15 @@ class Calculator
       inputs = inputs.gsub(';', ',')
     end
     input_array = inputs.gsub("\n", ",").split(",").map(&:to_i)
+    find_negatives(input_array)  #find out negative numbers
+    input_array.sum
+  end
+
+  private
+
+  def find_negatives(input_array)
     negatives = input_array.select { |number| number < 0 }
     raise "Negative numbers are not allowed: #{negatives.join(', ')}" unless negatives.empty?
-    input_array.sum
   end
 
 end
